712d3a71581a88abbbff344604e6749cdaebb07d
IgBovt <140014814+IgBovt@users.noreply.github.com>
2023-10-07 23:36:38 +0300

    finish

--- task-1.js
+++ task-1.js
@@ -8,6 +8,9 @@ function makeTransaction(quantity, pricePerDroid, customerCredits) {
         return `You ordered ${quantity} droids worth ${totalPrice} credits!`
     }
 
+    //  =========== ANOTHER WAY ================//
+    
+    // return totalPrice > customerCredits ? "Insufficient funds!" : `You ordered ${quantity} droids worth ${totalPrice} credits!`
 }
 
 console.log(makeTransaction(5, 3000, 23000)); // "You ordered 5 droids worth 15000 credits!"
